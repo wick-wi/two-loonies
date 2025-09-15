3316462f5a4a4b71c520efccd85e663df93ae1a6
Rasitha Wijesekera <rasithaw@Rasithas-Air.mynet>
2025-09-15 23:05:53 +0100

    Initial commit: Two Loonies landing page with React and Tailwind CSS

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import LandingPage from './LandingPage';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <LandingPage />
+    </div>
+  );
+}
+
+export default App;
